8228c9198d0be6312641a92f8109c3c077fede81
pranay8873 <pranaypuvaati@gmail.com>
2026-05-28 22:49:32 +0530

    fixed some errors in code

---
+++ src/main/java/com/java/spring/config2/AppConfig.java
@@ -0,0 +1,10 @@
+package com.java.spring.config2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "com.java.spring")
+public class AppConfig {
+}
